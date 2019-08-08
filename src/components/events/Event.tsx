69b33aa06cf1efdaf44998944fdced9ac50aca15
akolybelnikov <a.kolybelnikov@gmail.com>
2019-08-08 19:47:19 +0200

    Formatting the duration with date-fns

--- src/components/events/Event.tsx
+++ src/components/events/Event.tsx
@@ -9,6 +9,7 @@ import Flex from '../shared/Flex';
 import FlexRow from '../shared/FlexRow';
 import SubTitle from '../shared/SubTitle';
 import Text from '../shared/Text';
+import { differenceInMinutes } from 'date-fns'
 
 const RowItem = styled(Box).attrs({
   padding: [1, 2]
@@ -17,6 +18,12 @@ const RowItem = styled(Box).attrs({
 export default ({ event, city }: { event: Event, city: City }) => {
 
   const { startDate, name, isFree, endDate } = event
+
+  const formatDuration = (minutes: number): string => {
+    const hours: number = Math.floor(minutes / 60)
+    const rest: number = minutes % 60
+    return rest !== 0 ? `${hours}h ${rest}m` : `${hours}h`
+  }
 
   return (
     <FlexRow>
@@ -32,7 +39,9 @@ export default ({ event, city }: { event: Event, city: City }) => {
             <Box sx={{ flex: '50%' }}><Text>{city && city.name}</Text></Box>
             <Box sx={{ flex: 'auto' }}>
               <Text>
-                {startDate && endDate && <Moment diff={startDate} unit="minutes" decimal>{endDate}</Moment>} m
+                {startDate && endDate && formatDuration(
+                  differenceInMinutes(endDate, startDate)
+                )}
               </Text>
             </Box>
           </Flex>
@@ -41,7 +50,7 @@ export default ({ event, city }: { event: Event, city: City }) => {
           <SubTitle ml={1}>{isFree && <Text color="darkGreen">Free!!!</Text>}</SubTitle>
         </Box>
       </RowItem>
-      <RowItem sx={{ flex: ['40% 0 0', 'auto'], display: 'flex', justifyContent: 'flex-end' }}>
+      <RowItem sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
         <Button variant='primaryInverted'>Sign up</Button>
       </RowItem>
     </FlexRow>
