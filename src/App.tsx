302a831e760fcea18f00324d91fa2eb35ed0d096
akolybelnikov <a.kolybelnikov@gmail.com>
2020-02-23 02:44:51 +0100

    Replace localhost api with heroku server api.

--- src/App.tsx
+++ src/App.tsx
@@ -30,97 +30,97 @@ const GlobalStyle = createGlobalStyle`
 `
 
 const App: React.FC = () => {
-  const [events, setEvents] = useState<TechEvent[]>([])
-  const [userEvents, setUserEvents] = useState<TechEvent[]>([])
-  const [cities, setCities] = useState<City[]>([])
-  const [searchView, setView] = useState<boolean>(false)
-  const [filteredEvents, setFilteredEvents] = useState<TechEvent[]>([])
+    const [events, setEvents] = useState<TechEvent[]>([])
+    const [userEvents, setUserEvents] = useState<TechEvent[]>([])
+    const [cities, setCities] = useState<City[]>([])
+    const [searchView, setView] = useState<boolean>(false)
+    const [filteredEvents, setFilteredEvents] = useState<TechEvent[]>([])
 
-  useEffect(() => {
-    async function fetchData() {
-      const cities: AxiosResponse = await axios('http://localhost:3001/cities')
-      setCities(cities.data)
+    useEffect(() => {
+        async function fetchData() {
+            const cities: AxiosResponse = await axios('https://tech-meets-api.herokuapp.com/cities')
+            setCities(cities.data)
 
-      const events: AxiosResponse = await axios('http://localhost:3001/events')
-      setEvents(events.data.sort(
-        (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
-      ))
+            const events: AxiosResponse = await axios('https://tech-meets-api.herokuapp.com/events')
+            setEvents(events.data.sort(
+                (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
+            ))
 
-      const user: AxiosResponse = await axios('http://localhost:3001/user')
-      setUserEvents(user.data.sort(
-        (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
-      ))
+            const user: AxiosResponse = await axios('https://tech-meets-api.herokuapp.com/user')
+            setUserEvents(user.data.sort(
+                (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
+            ))
 
-      setFilteredEvents(events.data.sort(
-        (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
-      ))
-    }
-
-    fetchData()
-  }, [])
-
-  useEffect(() => {
-    const filtersSubscription: Subscription = $filteredEvents.subscribe(
-      curr => setFilteredEvents(curr)
-    )
-
-    return () => {
-      filtersSubscription.unsubscribe()
-    }
-  }, [])
-
-  const fetchUserEvents = async () => {
-    const user: AxiosResponse = await axios('http://localhost:3001/user')
-      const sorted = user.data.sort(
-        (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
-      )
-      setUserEvents(sorted)
-      setView(false)
-
-      const filtersSubscription: Subscription = $filters.subscribe((curr) => {
-        let filteredEvents: TechEvent[]
-        if (curr.view === 'my') {
-          filteredEvents = applyFilters(sorted, curr)
-          filtersService.setEvents(filteredEvents)
+            setFilteredEvents(events.data.sort(
+                (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
+            ))
         }
-      })
 
-      filtersSubscription.unsubscribe()
-  }
+        fetchData()
+    }, [])
 
-  const leaveEvent = async (id: number) => {
-    try {
-      await axios.delete(`http://localhost:3001/user/${id}`)   
-      fetchUserEvents()
-    } catch (e) {
-      console.error(e)
+    useEffect(() => {
+        const filtersSubscription: Subscription = $filteredEvents.subscribe(
+            curr => setFilteredEvents(curr)
+        )
+
+        return () => {
+            filtersSubscription.unsubscribe()
+        }
+    }, [])
+
+    const fetchUserEvents = async () => {
+        const user: AxiosResponse = await axios('https://tech-meets-api.herokuapp.com/user')
+        const sorted = user.data.sort(
+            (a: TechEvent, b: TechEvent) => (a.startDate > b.startDate) ? 1 : -1
+        )
+        setUserEvents(sorted)
+        setView(false)
+
+        const filtersSubscription: Subscription = $filters.subscribe((curr) => {
+            let filteredEvents: TechEvent[]
+            if (curr.view === 'my') {
+                filteredEvents = applyFilters(sorted, curr)
+                filtersService.setEvents(filteredEvents)
+            }
+        })
+
+        filtersSubscription.unsubscribe()
     }
-  }
 
-  return (
-    <ThemeProvider theme={AppTheme}>
-      <Container>
-        <Head />
-        <GlobalStyle />
-        <Container sx={{ margin: '0 auto' }} px={[2, 3]} pt={['5.5rem', 6]} width={['100%', '95%', '760px']}>
-          <Filters
-            events={events}
-            userEvents={userEvents}
-            setView={setView}
-          />
-        </Container>
-        <Container sx={{ margin: '0 auto' }} px={[1, 2, 3]} py={[1]} width={['100%', '95%', '760px']}>
-          <Events
-            searchView={searchView}
-            events={filteredEvents}
-            cities={cities}
-            userEvents={userEvents}
-            fetchUserEvents={fetchUserEvents}
-            leaveEvent={leaveEvent} />
-        </Container>
-      </Container>
-    </ThemeProvider>
-  )
+    const leaveEvent = async (id: number) => {
+        try {
+            await axios.delete(`https://tech-meets-api.herokuapp.com/user/${id}`)
+            fetchUserEvents()
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    return (
+        <ThemeProvider theme={AppTheme}>
+            <Container>
+                <Head />
+                <GlobalStyle />
+                <Container sx={{ margin: '0 auto' }} px={[2, 3]} pt={['5.5rem', 6]} width={['100%', '95%', '760px']}>
+                    <Filters
+                        events={events}
+                        userEvents={userEvents}
+                        setView={setView}
+                    />
+                </Container>
+                <Container sx={{ margin: '0 auto' }} px={[1, 2, 3]} py={[1]} width={['100%', '95%', '760px']}>
+                    <Events
+                        searchView={searchView}
+                        events={filteredEvents}
+                        cities={cities}
+                        userEvents={userEvents}
+                        fetchUserEvents={fetchUserEvents}
+                        leaveEvent={leaveEvent} />
+                </Container>
+            </Container>
+        </ThemeProvider>
+    )
 }
 
 export default App;
--- src/components/events/EventRow.tsx
+++ src/components/events/EventRow.tsx
@@ -16,143 +16,143 @@ import Title from '../shared/Title';
 import customStyles from './events.module.scss';
 
 export default (
-  { event, joined, fetchUserEvents, leaveEvent }:
-    { event: TechEvent, joined: boolean, fetchUserEvents: Function, leaveEvent: Function }) => {
-  const [open, setModal] = useState(false)
-  const { startDate, name, isFree, endDate } = event
+    { event, joined, fetchUserEvents, leaveEvent }:
+        { event: TechEvent, joined: boolean, fetchUserEvents: Function, leaveEvent: Function }) => {
+    const [open, setModal] = useState(false)
+    const { startDate, name, isFree, endDate } = event
 
-  const formatDuration = (minutes: number): string => {
-    const hours: number = Math.floor(minutes / 60)
-    const rest: number = minutes % 60
-    return rest !== 0 ? `${hours}h ${rest}m` : `${hours}h`
-  }
-
-  const onOpenModal = () => setModal(true)
-
-  const onCloseModal = () => setModal(false)
-
-  const onJoinEvent = async () => {
-    try {
-      await axios.post('http://localhost:3001/user', event)
-      await fetchUserEvents()
-      setModal(false)
-    } catch (e) {
-      setModal(false)
+    const formatDuration = (minutes: number): string => {
+        const hours: number = Math.floor(minutes / 60)
+        const rest: number = minutes % 60
+        return rest !== 0 ? `${hours}h ${rest}m` : `${hours}h`
     }
-  }
 
-  const onLeaveEvent = () => {
-    leaveEvent(event.id)
-  }
+    const onOpenModal = () => setModal(true)
 
-  return (
-    <Fragment>
-      <FlexRow style={{ zIndex: 1 }}>
-        <Box p={[1, 2]} sx={{ flex: ['15%', '10%'] }}>
-          {startDate && <Moment utc format="HH:mm">
-            {startDate}
-          </Moment>}
-        </Box>
-        <Box p={[1, 2]} sx={{
-          display: 'flex',
-          flex: ['80%', '70%'],
-          justifyContent: 'flex-start'
-        }}>
-          <Box sx={{ flex: ['60%'] }}>
-            <SubTitle>{name && name}</SubTitle>
-            <Box mt={[1]} sx={{ flex: '50%' }}>
-              <Text>{event.cityName} - {startDate && endDate && formatDuration(
-                differenceInMinutes(endDate, startDate)
-              )}</Text>
-            </Box>
-          </Box>
-          <Box sx={{
-            flex: '10%',
-            textAlign: 'left',
-            display: 'flex',
-            alignItems: 'center'
-          }}>
-            <SubTitle ml={1}>
-              {isFree && !joined && <Text color="darkGreen">Free!!!</Text>}
-              {joined && <Text color="darkGreen">You're in!</Text>}
-            </SubTitle>
-          </Box>
-        </Box>
-        <Box p={[1, 2]} sx={{
-          display: 'flex',
-          flexDirection: ['row', 'column'],
-          justifyContent: ['space-around'],
-          flex: '1 1 auto',
-          alignItems: ['center']
-        }}>
-          {!joined &&
-            <Button variant='primaryInverted' onClick={onOpenModal}>
-              Sign up
+    const onCloseModal = () => setModal(false)
+
+    const onJoinEvent = async () => {
+        try {
+            await axios.post('https://tech-meets-api.herokuapp.com/user', event)
+            await fetchUserEvents()
+            setModal(false)
+        } catch (e) {
+            setModal(false)
+        }
+    }
+
+    const onLeaveEvent = () => {
+        leaveEvent(event.id)
+    }
+
+    return (
+        <Fragment>
+            <FlexRow style={{ zIndex: 1 }}>
+                <Box p={[1, 2]} sx={{ flex: ['15%', '10%'] }}>
+                    {startDate && <Moment utc format="HH:mm">
+                        {startDate}
+                    </Moment>}
+                </Box>
+                <Box p={[1, 2]} sx={{
+                    display: 'flex',
+                    flex: ['80%', '70%'],
+                    justifyContent: 'flex-start'
+                }}>
+                    <Box sx={{ flex: ['60%'] }}>
+                        <SubTitle>{name && name}</SubTitle>
+                        <Box mt={[1]} sx={{ flex: '50%' }}>
+                            <Text>{event.cityName} - {startDate && endDate && formatDuration(
+                                differenceInMinutes(endDate, startDate)
+                            )}</Text>
+                        </Box>
+                    </Box>
+                    <Box sx={{
+                        flex: '10%',
+                        textAlign: 'left',
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
+                        <SubTitle ml={1}>
+                            {isFree && !joined && <Text color="darkGreen">Free!!!</Text>}
+                            {joined && <Text color="darkGreen">You're in!</Text>}
+                        </SubTitle>
+                    </Box>
+                </Box>
+                <Box p={[1, 2]} sx={{
+                    display: 'flex',
+                    flexDirection: ['row', 'column'],
+                    justifyContent: ['space-around'],
+                    flex: '1 1 auto',
+                    alignItems: ['center']
+                }}>
+                    {!joined &&
+                        <Button variant='primaryInverted' onClick={onOpenModal}>
+                            Sign up
             </Button>
-          }
-          {joined &&
-            <Button variant='primary' onClick={onLeaveEvent}>
-              Leave
+                    }
+                    {joined &&
+                        <Button variant='primary' onClick={onLeaveEvent}>
+                            Leave
             </Button>
-          }
-        </Box>
-      </FlexRow>
-      <Modal
-        open={open}
-        onClose={onCloseModal}
-        center
-        animationDuration={500}
-        classNames={{
-          transitionEnter: customStyles.transitionEnter,
-          transitionEnterActive: customStyles.transitionEnterActive,
-          transitionExit: customStyles.transitionExitActive,
-          transitionExitActive: customStyles.transitionExitActive,
-          overlay: customStyles.customOverlay,
-          modal: customStyles.customModal,
-        }}
-      >
-        <Card>
-          <Flex
-            py={[3]}
-            sx={{
-              justifyContent: 'center',
-              background: 'lightGreen'
-            }}>
-            <Title>Join the event</Title>
-          </Flex>
-          <FlexContainer minHeight={'35vh'} p={[2, 3, 4]}>
-            <SubTitle style={{ display: 'inline-block' }}>
-              {`You are about to sign up for `}
+                    }
+                </Box>
+            </FlexRow>
+            <Modal
+                open={open}
+                onClose={onCloseModal}
+                center
+                animationDuration={500}
+                classNames={{
+                    transitionEnter: customStyles.transitionEnter,
+                    transitionEnterActive: customStyles.transitionEnterActive,
+                    transitionExit: customStyles.transitionExitActive,
+                    transitionExitActive: customStyles.transitionExitActive,
+                    overlay: customStyles.customOverlay,
+                    modal: customStyles.customModal,
+                }}
+            >
+                <Card>
+                    <Flex
+                        py={[3]}
+                        sx={{
+                            justifyContent: 'center',
+                            background: 'lightGreen'
+                        }}>
+                        <Title>Join the event</Title>
+                    </Flex>
+                    <FlexContainer minHeight={'35vh'} p={[2, 3, 4]}>
+                        <SubTitle style={{ display: 'inline-block' }}>
+                            {`You are about to sign up for `}
+                        </SubTitle>
+                        <SubTitle style={{ display: 'inline-block' }} color="orange">
+                            {name}
+                        </SubTitle>
+                        <SubTitle style={{ display: 'inline-block' }}>
+                            {` This event takes place on the `}
+                            {startDate && <Moment format="Do MMMM">{startDate}</Moment>}
+                            {` in ${event.cityName}.`}<br /><br />Are you sure?
             </SubTitle>
-            <SubTitle style={{ display: 'inline-block' }} color="orange">
-              {name}
-            </SubTitle>
-            <SubTitle style={{ display: 'inline-block' }}>
-              {` This event takes place on the `}
-              {startDate && <Moment format="Do MMMM">{startDate}</Moment>}
-              {` in ${event.cityName}.`}<br /><br />Are you sure?
-            </SubTitle>
-          </FlexContainer>
-          <Flex py={[3]} sx={{ justifyContent: 'flex-end' }}>
-            <Box sx={{ flex: ['30% 0 0', '25% 0 0'], textAlign: 'center' }}>
-              <Button
-                minWidth={[84]}
-                variant='primaryInverted'
-                onClick={onCloseModal}>
-                Cancel
+                    </FlexContainer>
+                    <Flex py={[3]} sx={{ justifyContent: 'flex-end' }}>
+                        <Box sx={{ flex: ['30% 0 0', '25% 0 0'], textAlign: 'center' }}>
+                            <Button
+                                minWidth={[84]}
+                                variant='primaryInverted'
+                                onClick={onCloseModal}>
+                                Cancel
                 </Button>
-            </Box>
-            <Box sx={{ flex: ['30% 0 0', '25% 0 0'], textAlign: 'center' }}>
-              <Button
-                minWidth={[84]}
-                variant='secondaryInverted'
-                onClick={onJoinEvent}>
-                Join
+                        </Box>
+                        <Box sx={{ flex: ['30% 0 0', '25% 0 0'], textAlign: 'center' }}>
+                            <Button
+                                minWidth={[84]}
+                                variant='secondaryInverted'
+                                onClick={onJoinEvent}>
+                                Join
                 </Button>
-            </Box>
-          </Flex>
-        </Card>
-      </Modal>
-    </Fragment>
-  )
+                        </Box>
+                    </Flex>
+                </Card>
+            </Modal>
+        </Fragment>
+    )
 }
